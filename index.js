b313accbbc38e27c64d4e809c3be1e2c26635685
Ayomide Taiwo <tosin0330@gmail.com>
2023-04-21 22:17:31 -0500

    myfiles.md

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+
+function findMatching(drivers, query) {
+  return drivers.filter(driver => driver.toLowerCase().includes(query.toLowerCase()));
+}
+
+function fuzzyMatch(drivers, query) {
+  return drivers.filter(driver => driver.toLowerCase().startsWith(query.toLowerCase()));
+}
+
+function matchName(drivers, query) {
+  return drivers.filter(driver => driver.name === query);
+}
+
